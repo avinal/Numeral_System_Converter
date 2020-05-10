8bd8a9286165e2b618e645087236efddb43fd620
Avinal <avinal.xlvii@gmail.com>
2020-05-10 15:20:34 +0530

    Added javadoc

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"converter","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"converter","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"converter","c":"Main","l":"toBase(String, int)","url":"toBase(java.lang.String,int)"},{"p":"converter","c":"Main","l":"toDecimal(String, int)","url":"toDecimal(java.lang.String,int)"}]
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"converter","l":"Main"}]
