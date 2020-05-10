4fa9b3efadafa30afacd939d4f800e7bf62f6988
Avinal <avinal.xlvii@gmail.com>
2020-05-10 14:59:19 +0530

    Added method comments.

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -2,10 +2,18 @@ package converter;
 
 import java.util.Scanner;
 
+/**
+ * @author Avinal Kumar
+ * @since May 10th, 2020
+ * We’re all quite used to our good old decimal system of numerals. But let’s
+ * not forget that there are countless other ways to count! Whether we convert numbers
+ * from one system to another just for fun or to store large data more efficiently,
+ * a converter would be helpful. In this project I created a mathematical helper
+ * that will help us convert numbers from system M to system N.
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         try {
             int rads = in.nextInt();
             in.nextLine();
@@ -22,6 +30,20 @@ public class Main {
         in.close();
     }
 
+    /**
+     * Converts number from any base to Decimal Number System. Converts whole
+     * numbers as well as floating point number. Number without floating point are
+     * directly converted using <strong>Integer.parseInt</strong> and
+     * <strong>Integer.toString</strong> methods. Numbers with floating points are
+     * converted using algorithm.
+     *
+     * @param num   Number to be converted.
+     * @param radix Radix of the number to be converted.
+     * @return Converted Decimal Number string.
+     * @see java.lang.Integer#parseInt(String, int)
+     * @see java.lang.Integer#toString(int, int)
+     * @see java.lang.String#split(String)
+     */
     public static String toDecimal(String num, int radix) {
         String[] parts;
         double decimal;
@@ -46,10 +68,22 @@ public class Main {
         return Double.toString(decimal);
     }
 
+    /**
+     * Converts a decimal number system to N base system. Whole Numbers as well as n
+     * base floating number.Number without floating point are directly converted
+     * using <strong>Integer.parseInt</strong> and <strong>Integer.toString</strong>
+     * methods. Numbers with floating points are converted using algorithm.
+     *
+     * @param num   Number in Decimal Number System.
+     * @param radix Target radix in which to convert.
+     * @return Converted N base Numeral
+     * @see java.lang.Double#parseDouble(String)
+     * @see java.lang.Double#toHexString(double)
+     */
     public static String toBase(String num, int radix) {
         String[] parts = num.split("[.]");
         if (radix == 1) {
-            return new String("1").repeat(Integer.parseInt(parts[0]));
+            return "1".repeat(Integer.parseInt(parts[0]));
         }
         if (parts.length == 1) {
             return Integer.toString(Integer.parseInt(parts[0]), radix);
